b4a2e0b9591d73a8b7a1c143a6c03e73d38187f9
Olena1iashenko <lyashenko.alenkaa@gmail.com>
2025-06-16 19:01:53 +0300

    connect MongoDB with mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,43 +1,40 @@
 import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import boardRouter from './routes/boardRouter';
 import taskRouter from './routes/taskRouter';
 import { NextFunction, Request, Response } from 'express';
 import HttpError from './helpers/HttpError';
+import { env } from './helpers/env';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+const startServer = async () => {
+  const app = express();
+  const PORT = env('PORT');
 
-app.use(cors());
-app.use(express.json());
+  app.use(cors());
+  app.use(express.json());
 
-app.use('/api', boardRouter);
-app.use('/api', taskRouter);
+  app.use('/api', boardRouter);
+  app.use('/api', taskRouter);
 
-app.use((_, res) => {
-  res.status(404).json({ message: 'Route not found' });
-});
+  app.use((_, res) => {
+    res.status(404).json({ message: 'Route not found' });
+  });
 
-app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof HttpError) {
-    res.status(err.statusCode).json({ message: err.message });
-  } else {
-    console.log(err);
-    res.status(500).json({ message: 'An unexpected error occurred' });
-  }
-});
+  app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof HttpError) {
+      res.status(err.statusCode).json({ message: err.message });
+    } else {
+      console.log(err);
+      res.status(500).json({ message: 'An unexpected error occurred' });
+    }
+  });
 
-app.listen(PORT, () => {
-  console.log(`server started on ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`server started on ${PORT}`);
+  });
+};
 
-mongoose
-  .connect(process.env.MONGO_URI!)
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch((err) => console.error('MongoDB connection error:', err));
+export default startServer;
